90abbe9f2dd0d07fa5ef3a9deeee5c820b05f335
Kor de Jong <k.dejong1@uu.nl>
2016-09-15 17:19:43 +0200

    Wip

---
+++ source/lue/python_api/test/time/omnipresent/variable_shape/use_case_test.py
@@ -0,0 +1,81 @@
+import numpy
+import lue
+import lue_test
+
+
+class UseCaseTest(lue_test.TestCase):
+
+
+    def xxxxxtest_use_case_1(self):
+
+        dataset_name = "o_vs_use_case_1.lue"
+        dataset = self.create_dataset(dataset_name)
+        universe = dataset.add_universe("universe")
+        phenomenon = universe.add_phenomenon("phenomenon")
+
+
+        # Add a new property set. This returns a type with a special API,
+        # which is dependent on the item/time domain configuration.
+        space_configuration = lue.SpaceDomainConfiguration(
+            lue.space_domain.located, lue.space_domain_item.box)
+        property_set = phenomenon.add_property_set("property_set",
+            space_configuration)
+
+        self.assertEqual(property_set.domain.configuration.time.type,
+            lue.time_domain.omnipresent)
+        self.assertEqual(property_set.domain.configuration.space.type,
+            lue.space_domain.located)
+        self.assertEqual(property_set.domain.configuration.space.item_type,
+            lue.space_domain_item.box)
+        self.assertEqual(property_set.items.dtype, numpy.uint64)
+        self.assertEqual(len(property_set.items.shape), 1)
+        self.assertEqual(property_set.items.shape[0], 0)
+
+        ### rank = 2
+
+        ### space_domain = property_set.domain.space_domain
+        ### self.assertEqual(space_domain.items.dtype, numpy.float64)
+        ### self.assertEqual(len(space_domain.items.shape), 2)
+        ### self.assertEqual(space_domain.items.shape[0], 0)
+        ### self.assertEqual(space_domain.items.shape[1], rank * 2**rank)
+
+
+        ### self.assertEqual(property_set.domain.space_domain.reserve_items(nr_items))
+
+
+        ### # Add items. This is independent of whether or there are properties
+        ### # added to the set.
+        ### nr_items = 500
+        ### items = property_set.reserve_items(nr_items)
+
+        ### self.assertEqual(items.shape[0], nr_items)
+
+        ### items_ = numpy.array([id for id in xrange(nr_items)], numpy.uint64)
+        ### items[:] = items_
+
+        ### self.assertArraysEqual(items[:], items_)
+
+
+        ### # Now, add a property, whose values all have different shapes.
+        ### ### value_shape = (4, 5)
+        ### ### chunk_shape = (4, 5)
+        ### value_type = numpy.int32
+        ### property = property_set.add_property("property", value_type)
+        ### self.assertEqual(property.name, "property")
+        ### # self.assertEqual(property.values.dtype, numpy.int32)
+
+        ### ### self.assertEqual(len(property.values.shape), 3)
+        ### ### self.assertEqual(property.values.shape[0], 0)
+        ### ### self.assertEqual(property.values.shape[1], 4)
+        ### ### self.assertEqual(property.values.shape[2], 5)
+
+        ### ### values = property.reserve_items(nr_items)
+
+        ### ### self.assertEqual(values.shape[0], nr_items)
+
+        ### ### values_ = numpy.arange(nr_items * reduce(lambda x, y: x * y, value_shape),
+        ### ###     dtype=numpy.int32).reshape((nr_items,) + value_shape)
+
+        ### ### values[:] = values_
+
+        ### ### self.assertArraysEqual(values[:], values_)
