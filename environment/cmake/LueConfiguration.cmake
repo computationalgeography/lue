fe9d456c0fe2145512b26f2eee5a5f272a149930
Kor de Jong <k.dejong1@uu.nl>
2017-08-30 13:59:08 +0200

    Wip

--- environment/cmake/LueConfiguration.cmake
+++ environment/cmake/LueConfiguration.cmake
@@ -1,4 +1,4 @@
-# Short-cuts.
+# Short-cuts
 option(LUE_BUILD_ALL
     "Build everything, except for documentation and tests"
     FALSE)
@@ -6,7 +6,7 @@ option(LUE_WITH_ALL
     "Support all features, except for MPI"
     FALSE)
 
-# High level targets.
+# High level targets
 option(LUE_BUILD_PYTHON_API
     "Build Python-API (implies LUE_BUILD_CXX_API)"
     FALSE)
@@ -14,15 +14,18 @@ option(LUE_BUILD_UTILITIES
     "Build LUE command line utilites (implies LUE_BUILD_HL_API)"
     TRUE)
 
-# Lower level targets.
+# Lower level targets
 option(LUE_BUILD_HL_API
     "Build HL-API (implies LUE_BUILD_CXX_API)"
     TRUE)
 option(LUE_BUILD_CXX_API
-    "Build CXX-API"
+    "Build CXX-API (implies LUE_BUILD_HDF5_API)"
+    TRUE)
+option(LUE_BUILD_HDF5_API
+    "Build C++ wrapper library around HDF5 C-API"
     TRUE)
 
-# Targets relevant for developers.
+# Targets relevant for developers
 option(LUE_BUILD_DOCUMENTATION
     "Build documentation"
     FALSE)
@@ -30,13 +33,13 @@ option(LUE_BUILD_TEST
     "Build tests"
     FALSE)
 
-# Optional features.
+# Optional features
 option(LUE_API_WITH_MPI
     "Include support for MPI"
     FALSE)
 
 
-# Handle internal dependencies.
+# Handle internal dependencies
 if(LUE_BUILD_ALL)
     set(LUE_BUILD_PYTHON_API TRUE)
     set(LUE_BUILD_UTILITIES TRUE)
@@ -54,6 +57,10 @@ if(LUE_BUILD_HL_API)
     set(LUE_BUILD_CXX_API TRUE)
 endif()
 
+if(LUE_BUILD_CXX_API)
+    set(LUE_BUILD_HDF5_API TRUE)
+endif()
+
 if(LUE_BUILD_TEST)
     set(DEVBASE_BUILD_TEST TRUE)
 endif()
@@ -66,14 +73,16 @@ endif()
 
 
 # Handle external dependencies.
+if(LUE_BUILD_HDF5_API)
+    set(DEVBASE_HDF5_REQUIRED TRUE)
+    list(APPEND DEVBASE_REQUIRED_HDF5_COMPONENTS
+        C)  # HL
+endif()
+
 if(LUE_BUILD_CXX_API)
     set(DEVBASE_BOOST_REQUIRED TRUE)
     list(APPEND DEVBASE_REQUIRED_BOOST_COMPONENTS
         filesystem system)
-
-    set(DEVBASE_HDF5_REQUIRED TRUE)
-    list(APPEND DEVBASE_REQUIRED_HDF5_COMPONENTS
-        C)  # HL
 
     if(LUE_API_WITH_MPI)
         set(DEVBASE_MPI_REQUIRED TRUE)
@@ -84,6 +93,7 @@ endif()
 if(LUE_BUILD_UTILITIES)
     set(DEVBASE_DOCOPT_REQUIRED TRUE)
     set(DEVBASE_GDAL_REQUIRED TRUE)
+    set(DEVBASE_REQUIRED_GDAL_VERSION 2)
     set(DEVBASE_NLOHMANN_JSON_REQUIRED TRUE)
 
     set(LUE_BUILD_LUE_DUMP TRUE)
