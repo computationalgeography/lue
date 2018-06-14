c66081e76c8b71314b24eb44d7ef21a1f3f6f6b5
Kor de Jong <k.dejong1@uu.nl>
2018-06-14 11:07:58 +0200

    Initial version of different shape x variable shape value

---
+++ source/lue_cxx/src/array/different_shape/variable_shape/value.cpp
@@ -0,0 +1,118 @@
+#include "lue/array/different_shape/variable_shape/value.hpp"
+#include "lue/core/tag.hpp"
+
+
+namespace lue {
+namespace different_shape {
+namespace variable_shape {
+
+/*!
+    @brief      Open value @a name in @a parent
+*/
+Value::Value(
+    hdf5::Group const& parent,
+    std::string const& name)
+:
+    ValueGroup{parent, name},
+    _nr_locations_in_time{attributes().read<Count>(nr_locations_in_time_tag)}
+
+{
+}
+
+
+/*!
+    @brief      Open value @a name in @a parent
+*/
+Value::Value(
+    hdf5::Group const& parent,
+    std::string const& name,
+    hdf5::Datatype const& memory_datatype)
+:
+    ValueGroup{parent, name, memory_datatype},
+    _nr_locations_in_time{attributes().read<Count>(nr_locations_in_time_tag)}
+
+{
+}
+
+
+Value::Value(
+    ValueGroup&& group)
+:
+    ValueGroup{std::forward<ValueGroup>(group)},
+    _nr_locations_in_time{attributes().read<Count>(nr_locations_in_time_tag)}
+
+{
+}
+
+
+Count Value::nr_locations_in_time() const
+{
+    return _nr_locations_in_time;
+}
+
+
+different_shape::Value Value::reserve(
+    Index const idx,
+    Count const nr_objects,
+    ID const* ids,
+    hdf5::Shape const* shapes)
+{
+    std::string const name = std::to_string(idx);
+    auto value = different_shape::create_value(
+        *this, name, file_datatype(), memory_datatype(), rank());
+
+    value.reserve(nr_objects, ids, shapes);
+
+    attributes().write<Count>(
+        nr_locations_in_time_tag, ++_nr_locations_in_time);
+
+    return value;
+}
+
+
+different_shape::Value Value::operator[](
+    Index const idx)
+{
+    std::string const name = std::to_string(idx);
+
+    return different_shape::Value{*this, name, memory_datatype()};
+}
+
+
+/*!
+    @brief      Create value @a name in @a parent
+*/
+Value create_value(
+    hdf5::Group& parent,
+    std::string const& name,
+    hdf5::Datatype const& memory_datatype,
+    Rank const rank)
+{
+    return create_value(
+        parent, name,
+        file_datatype(memory_datatype), memory_datatype,
+        rank);
+}
+
+
+/*!
+    @brief      Create value @a name in @a parent
+*/
+Value create_value(
+    hdf5::Group& parent,
+    std::string const& name,
+    hdf5::Datatype const& file_datatype,
+    hdf5::Datatype const& memory_datatype,
+    Rank const rank)
+{
+    auto group = create_value_group(
+        parent, name, file_datatype, memory_datatype, rank);
+
+    group.attributes().write<Count>(nr_locations_in_time_tag, 0);
+
+    return Value{std::move(group)};
+}
+
+}  // namespace variable_shape
+}  // namespace different_shape
+}  // namespace lue
---
+++ source/lue_cxx/test/array/different_shape/variable_shape/value_test.cpp
@@ -0,0 +1,124 @@
+#define BOOST_TEST_MODULE lue array different_shape variable_shape value
+#include <boost/test/unit_test.hpp>
+#include "lue/array/different_shape/variable_shape/value.hpp"
+#include "lue/test.hpp"
+
+
+class Fixture:
+    public lue::test::FileFixture
+{
+
+public:
+
+    Fixture()
+        : FileFixture{"value.h5"},
+          _filename{"value.h5"},
+          _value_name{"my_value"},
+          _datatype{lue::hdf5::NativeDatatypeTraits<int32_t>::type_id()},
+          _rank{2},
+          _file{std::make_unique<lue::hdf5::File>(
+            lue::hdf5::create_file(_filename))},
+          _value{std::make_unique<lue::different_shape::variable_shape::Value>(
+            lue::different_shape::variable_shape::create_value(
+                *_file, _value_name, _datatype, _rank))}
+    {
+    }
+
+    ~Fixture()
+    {
+    }
+
+    auto& value()
+    {
+        return *_value;
+    }
+
+    auto rank() const
+    {
+        return _rank;
+    }
+
+    auto const& datatype() const
+    {
+        return _datatype;
+    }
+
+private:
+
+    std::string const _filename;
+    std::string const _value_name;
+    lue::hdf5::Datatype const _datatype;
+    lue::Rank const _rank;
+    std::unique_ptr<lue::hdf5::File> _file;
+    std::unique_ptr<lue::different_shape::variable_shape::Value> _value;
+
+};
+
+
+BOOST_FIXTURE_TEST_CASE(create_value, Fixture)
+{
+    auto const& value = this->value();
+
+    BOOST_CHECK(value.memory_datatype() == datatype());
+    BOOST_CHECK(
+        value.file_datatype() == lue::hdf5::file_datatype(datatype()));
+    BOOST_CHECK_EQUAL(value.rank(), rank());
+    BOOST_CHECK_EQUAL(value.nr_locations_in_time(), 0);
+}
+
+
+BOOST_FIXTURE_TEST_CASE(update_all_object_arrays_per_location_in_time, Fixture)
+{
+    auto& value = this->value();
+
+    lue::Count const nr_locations_in_time = 3;
+    lue::Counts const nr_active_objects{3, 4, 2};
+    std::vector<lue::IDs> const active_ids{
+        {5, 6, 7},  // 3 active objects
+        {5, 6, 8, 9},  // 4
+        {8, 3}  // 2
+    };
+    std::vector<lue::Shapes> const array_shapes{
+        {{3, 2}, {5, 4}, {7, 6}},  // 3 active objects
+        {{5, 4}, {7, 6}, {3, 2}, {6, 5}},  // 4
+        {{7, 6}, {3, 2}}  // 2
+    };
+
+    for(std::size_t t = 0; t < nr_locations_in_time; ++t) {
+        auto t_value = value.reserve(
+            t, nr_active_objects[t], active_ids[t].data(),
+            array_shapes[t].data());
+
+        BOOST_REQUIRE_EQUAL(t_value.nr_objects(), nr_active_objects[t]);
+
+        for(std::size_t o = 0; o < t_value.nr_objects(); ++o) {
+            auto array = t_value[active_ids[t][o]];
+
+            BOOST_CHECK(array.memory_datatype() == datatype());
+            BOOST_CHECK(
+                array.file_datatype() == lue::hdf5::file_datatype(datatype()));
+            BOOST_CHECK(array.shape() == array_shapes[t][o]);
+        }
+    }
+
+    BOOST_REQUIRE_EQUAL(value.nr_locations_in_time(), nr_locations_in_time);
+
+    for(std::size_t t = 0; t < nr_locations_in_time; ++t) {
+        auto t_value = value[t];
+
+        BOOST_REQUIRE_EQUAL(t_value.nr_objects(), nr_active_objects[t]);
+
+        for(std::size_t o = 0; o < t_value.nr_objects(); ++o) {
+            auto array = t_value[active_ids[t][o]];
+
+            BOOST_CHECK(array.memory_datatype() == datatype());
+            BOOST_CHECK(
+                array.file_datatype() == lue::hdf5::file_datatype(datatype()));
+            BOOST_CHECK(array.shape() == array_shapes[t][o]);
+        }
+    }
+
+    // Reading and writing elements is handled by the different_shape::Value
+    // instance returned by operator[]. This functionality is already
+    // tested elsewhere.
+}
