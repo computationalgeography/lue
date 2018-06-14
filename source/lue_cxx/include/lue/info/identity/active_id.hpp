6a0c15c34a04cd21a7410005389bdec843c496b4
Kor de Jong <k.dejong1@uu.nl>
2018-06-14 12:57:40 +0200

    Update doc

--- source/lue_cxx/include/lue/info/identity/active_id.hpp
+++ source/lue_cxx/include/lue/info/identity/active_id.hpp
@@ -4,6 +4,27 @@
 
 namespace lue {
 
+/*!
+    @brief      An ID per object in the active set
+    @param      .
+    @return     .
+    @exception  .
+
+    In the ActiveID dataset, IDs can be stored of objects that are active
+    at a given location in time. Multiple sets of object IDs can be stored
+    sequentially. The index of the ID of the first active object in a set
+    can be stored using the ActiveSetID class.
+
+    In case related information (IDs of the active objects, locations
+    in space, properties) does not change through time, only a single
+    set of IDs need to be stored.
+
+    Tracking the IDs of the active objects is needed in combination with
+    the folowing object array kinds:
+    - same shape x constant value
+    - same shape x constant shape
+    - same shape x variable shape
+*/
 class ActiveID:
     public same_shape::constant_shape::Value
 {
