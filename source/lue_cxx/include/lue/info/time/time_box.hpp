eb402c6454038e7efe245a9abf19b13ece938a31
Kor de Jong <k.dejong1@uu.nl>
2018-06-19 09:34:00 +0200

    Initial version of TimeBox class

---
+++ source/lue_cxx/include/lue/info/time/time_box.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "lue/array/same_shape/constant_shape/value.hpp"
+#include "lue/core/clock.hpp"
+
+
+namespace lue {
+
+/*!
+    - Zero or more time boxes per item
+    - Each time box has a unique location in time
+*/
+class TimeBox:
+    public same_shape::constant_shape::Value
+{
+
+public:
+
+                   TimeBox             (hdf5::Group const& parent);
+
+                   TimeBox             (TimeBox const&)=delete;
+
+                   TimeBox             (TimeBox&&)=default;
+
+                   TimeBox             (
+                                    same_shape::constant_shape::Value&& value);
+
+                   ~TimeBox            ()=default;
+
+    TimeBox&       operator=           (TimeBox const&)=delete;
+
+    TimeBox&       operator=           (TimeBox&&)=default;
+
+private:
+
+};
+
+
+TimeBox            create_time_box     (hdf5::Group& parent,
+                                        lue::Clock const& clock);
+
+}  // namespace lue
