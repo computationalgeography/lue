f23e8d6644a50fa82f2653c97c43bd053bcb705b
Kor de Jong <k.dejong1@uu.nl>
2018-06-19 16:18:16 +0200

    Initial version of different shape x variable shape Property

---
+++ source/lue_cxx/test/info/property/different_shape/variable_shape/property_test.cpp
@@ -0,0 +1,68 @@
+#define BOOST_TEST_MODULE lue info property different_shape variable_shape property
+#include <boost/test/unit_test.hpp>
+#include "lue/info/property/different_shape/variable_shape/property.hpp"
+#include "lue/test.hpp"
+
+
+class Fixture:
+    public lue::test::FileFixture
+{
+
+public:
+
+    Fixture():
+        FileFixture{"property.h5"},
+        _filename{"property.h5"},
+        _name{"my_property"},
+        _datatype{lue::hdf5::NativeDatatypeTraits<int32_t>::type_id()},
+        _rank{2},
+        _file{std::make_unique<lue::hdf5::File>(
+            lue::hdf5::create_file(_filename))},
+        _property{std::make_unique<lue::different_shape::variable_shape::Property>(
+            lue::different_shape::variable_shape::create_property(
+                *_file, _name, _datatype, _rank))}
+    {
+    }
+
+    ~Fixture()
+    {
+    }
+
+    auto& property()
+    {
+        return *_property;
+    }
+
+    auto const& name() const
+    {
+        return _name;
+    }
+
+    auto const& datatype() const
+    {
+        return _datatype;
+    }
+
+    auto rank() const
+    {
+        return _rank;
+    }
+
+private:
+
+    std::string const _filename;
+    std::string const _name;
+    lue::hdf5::Datatype const _datatype;
+    lue::Rank const _rank;
+    std::unique_ptr<lue::hdf5::File> _file;
+    std::unique_ptr<lue::different_shape::variable_shape::Property> _property;
+
+};
+
+
+BOOST_FIXTURE_TEST_CASE(create_property, Fixture)
+{
+    auto const& property = this->property();
+
+    BOOST_CHECK_EQUAL(property.name(), name());
+}
