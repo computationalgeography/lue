6b890d1effc844e4412e2db7344452cfd030ba35
Kor de Jong <k.dejong1@uu.nl>
2018-06-21 14:35:17 +0200

    Initial version of ID class

---
+++ source/lue_cxx/src/info/identity/id.cpp
@@ -0,0 +1,42 @@
+#include "lue/info/identity/id.hpp"
+#include "lue/core/tag.hpp"
+
+
+namespace lue {
+namespace info {
+
+ID::ID(
+    hdf5::Group const& parent)
+:
+    same_shape::Value{parent, id_tag}
+
+{
+}
+
+
+ID::ID(
+    same_shape::Value&& value)
+:
+    same_shape::Value{std::forward<same_shape::Value>(value)}
+
+{
+}
+
+
+Count ID::nr_objects() const
+{
+    return nr_arrays();
+}
+
+
+ID create_id(
+    hdf5::Group& parent)
+{
+    auto value = same_shape::create_value(
+        parent, id_tag, hdf5::Datatype{H5T_NATIVE_HSIZE});
+
+    return ID{std::move(value)};
+}
+
+}  // namespace info
+}  // namespace lue
