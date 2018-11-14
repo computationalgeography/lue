ac1a8196d4e1f6d029e308ed7dcd360fcc7de513
Kor de Jong <k.dejong1@uu.nl>
2018-11-14 15:24:18 +0100

    HPX use fixes

---
+++ source/lue_framework/algorithm/hpx/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/iostreams.hpp>
+
+
+int main()
+{
+    // Say hello to the world!
+    hpx::cout << "Hello World!\n" << hpx::flush;
+    return 0;
+}
