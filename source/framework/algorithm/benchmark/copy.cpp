264179f6659b481794c048aad9b58ed70ad3c297
Kor de Jong <k.dejong1@uu.nl>
2019-09-10 14:07:07 +0200

    Update copy algorithm

--- source/framework/algorithm/benchmark/copy.cpp
+++ source/framework/algorithm/benchmark/copy.cpp
@@ -1,6 +1,7 @@
 #include "copy.hpp"
 #include "lue/framework/core/component/partitioned_array.hpp"
 #include "lue/framework/algorithm/copy.hpp"
+#include <hpx/include/iostreams.hpp>
 
 
 namespace lue {
@@ -28,40 +29,24 @@ void copy(
         task.partition_shape().begin(), task.partition_shape().end(),
         partition_shape.begin());
 
-    // → Create first array
-    Array current_state{shape, partition_shape};
+    // → Create initial array
+    Array state{shape, partition_shape};
+    hpx::cout << describe(state) << hpx::endl;
 
-    assert(current_state.shape() == shape);
-
-    // FIXME
-    // auto fill_value = hpx::make_ready_future<Element>(5).share();
-    // fill(current_state, fill_value).wait();
+    assert(state.shape() == shape);
 
     for(std::size_t i = 0; i < task.nr_time_steps(); ++i) {
 
         // A very simple model: the new state is a copy of the current
         // state...
-        // new_state is a newly allocated array containing array
-        // partitions containing elements copied from current_state
-        // → Create second array on each iteration. Moved from copy result.
-        auto new_state = copy(current_state);
+        state = copy(state);
 
-        // Prepare for the next iteration
-        // The new current state is what is now the new state. The old
-        // current state can be overwritten. If all goes well, partitions
-        // not copied yet will remain in memory until they are not needed
-        // anymore. Moving from a partitioned array should not explicitly
-        // destruct component server instances.
-        current_state = std::move(new_state);
-
+        hpx::cout << '.' << hpx::flush;
     }
 
-    // FIXME
-    // // Test whether all values in the output array are equal to the fill
-    // // value used to fill the input array
-    // auto equal_to = lue::equal_to(current_state, fill_value);
-    // auto all = lue::all(equal_to);
-    // assert(all.get());
+    hpx::cout << "!" << hpx::flush;
+    hpx::wait_all(state.begin(), state.end());
+    hpx::cout << hpx::endl;
 }
 
 }  // namespace detail
@@ -78,31 +63,6 @@ void copy(
             break;
         }
     }
-
-    // using Array = PartitionedArray<Element, rank>;
-    // using Shape = typename Array::Shape;
-
-    // // Allocate array for initial state. The array is uninitialized,
-    // // which is fine.
-    // Shape shape{{nr_rows, nr_cols}};
-    // Array current_state{shape};
-
-    // for(std::size_t i = 0; i < nr_time_steps; ++i) {
-
-    //     // A very simple model: the new state is a copy of the current
-    //     // state...
-    //     // new_state is a newly allocated array containing array
-    //     // partitions containing elements copied from current_state
-    //     auto new_state = copy(current_state);
-
-    //     // Prepare for the next iteration
-    //     // The new current state is what is now the new state. The old
-    //     // current state can be overwritten. If all goes well, partitions
-    //     // not copied yet will remain in memory until they are not needed
-    //     // anymore. Moving from a partitioned array should not explicitly
-    //     // destruct component server instances.
-    //     current_state = std::move(new_state);
-    // }
 }
 
 }  // namespace benchmark
