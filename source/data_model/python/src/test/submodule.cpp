862ae8f56fdb58b281e6a08caa5be41d184cca1c
Kor de Jong <k.dejong1@uu.nl>
2020-08-19 12:19:45 +0200

    Fix failing macOS build

--- source/data_model/python/src/test/submodule.cpp
+++ source/data_model/python/src/test/submodule.cpp
@@ -27,11 +27,12 @@ void init_submodule(
         {
             gsl::span<Count> const active_set_sizes_{
                 const_cast<py::array_t<Count>&>(active_set_sizes).mutable_data(),
-                active_set_sizes.size()};
+                static_cast<gsl::span<Count>::size_type>(active_set_sizes.size())};
             gsl::span<Index> active_set_idxs_{
-                active_set_idxs.mutable_data(), active_set_idxs.size()};
+                active_set_idxs.mutable_data(),
+                static_cast<gsl::span<Index>::size_type>(active_set_idxs.size())};
             gsl::span<ID> active_ids_{
-                active_ids.mutable_data(), active_ids.size()};
+                active_ids.mutable_data(), static_cast<gsl::span<ID>::size_type>(active_ids.size())};
 
             select_random_ids(
                 active_set_sizes_, active_set_idxs_, active_ids_,
@@ -45,7 +46,7 @@ void init_submodule(
             Count const max_nr_objects)
         {
             gsl::span<ID> active_ids_{
-                active_ids.mutable_data(), active_ids.size()};
+                active_ids.mutable_data(), static_cast<gsl::span<ID>::size_type>(active_ids.size())};
 
             select_random_ids(active_ids_, max_nr_objects);
         });
