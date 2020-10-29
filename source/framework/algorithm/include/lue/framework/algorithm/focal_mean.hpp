210b9f22777fc6d031d7e3fc45d77696c5b3d4d1
Kor de Jong <k.dejong1@uu.nl>
2020-10-29 11:39:13 +0100

    Refactor focal operation

--- source/framework/algorithm/include/lue/framework/algorithm/focal_mean.hpp
+++ source/framework/algorithm/include/lue/framework/algorithm/focal_mean.hpp
@@ -21,61 +21,58 @@ public:
 
     using OutputElement = InputElement;
 
-    class Aggregator
-    {
-
-    public:
-
-        Aggregator():
-            _sum{0},
-            _sum_of_weights{0}
-        {
-        }
-
-        template<
-            typename Weight>
-        void add(
-            Weight const weight,
-            InputElement const value)
-        {
-            static_assert(std::is_same_v<Weight, bool> || std::is_floating_point_v<Weight>);
-
-            if constexpr(std::is_same_v<Weight, bool>) {
-                if(weight && !std::isnan(value)) {
-                    _sum += value;
-                    _sum_of_weights += 1.0;
-                }
-            }
-            else {
-                if(!std::isnan(value)) {
-                    _sum += weight * value;
-                    _sum_of_weights += weight;
-                }
-            }
-        }
-
-        OutputElement operator()() const
-        {
-            return _sum_of_weights > 0.0
-                ? _sum / _sum_of_weights
-                : std::numeric_limits<InputElement>::quiet_NaN();
-        }
-
-    private:
-
-        OutputElement _sum;
-
-        double _sum_of_weights;
-
-    };
-
-    FocalMean()
-    {
-    }
+    FocalMean()=default;
 
     constexpr InputElement fill_value() const
     {
         return std::numeric_limits<InputElement>::quiet_NaN();
+    }
+
+    template<
+        typename Subspan,
+        typename Kernel>
+    OutputElement operator()(
+        Subspan const& window,
+        Kernel const& kernel) const
+    {
+        static_assert(rank<Kernel> == 2);
+
+        using Weight = ElementT<Kernel>;
+
+        // TODO Add traits to grab typename of elements in Subspan
+        // static_assert(std::is_same_v<ElementT<Subspan>, InputElement>);
+        static_assert(std::is_same_v<Weight, bool> || std::is_floating_point_v<Weight>);
+
+        OutputElement sum{0};
+        double sum_of_weights{0};
+
+        lue_assert(window.extent(0) == kernel.size());
+        lue_assert(window.extent(1) == kernel.size());
+
+        for(Index r = 0; r < window.extent(0); ++r) {
+            for(Index c = 0; c < window.extent(1); ++c)
+            {
+                Weight const weight{kernel(r, c)};
+                InputElement const value{window(r, c)};
+
+                if constexpr(std::is_same_v<Weight, bool>) {
+                    if(weight && !std::isnan(value)) {
+                        sum += value;
+                        sum_of_weights += 1.0;
+                    }
+                }
+                else {
+                    if(!std::isnan(value)) {
+                        sum += weight * value;
+                        sum_of_weights += weight;
+                    }
+                }
+            }
+        }
+
+        return sum_of_weights > 0.0
+            ? sum / sum_of_weights
+            : std::numeric_limits<InputElement>::quiet_NaN();
     }
 
 };
--- source/framework/algorithm/include/lue/framework/algorithm/focal_sum.hpp
+++ source/framework/algorithm/include/lue/framework/algorithm/focal_sum.hpp
@@ -14,54 +14,56 @@ public:
 
     using OutputElement = InputElement;
 
-    class Aggregator
-    {
-
-    public:
-
-        Aggregator():
-            _sum{0}
-        {
-        }
-
-        template<
-            typename Weight>
-        void add(
-            Weight const weight,
-            InputElement const value)
-        {
-            static_assert(
-                std::is_same_v<Weight, bool> ||
-                std::is_floating_point_v<Weight>);
-
-            if constexpr(std::is_same_v<Weight, bool>) {
-                if(weight) {
-                    _sum += value;
-                }
-            }
-            else {
-                _sum += weight * value;
-            }
-        }
-
-        OutputElement operator()() const
-        {
-            return _sum;
-        }
-
-    private:
-
-        OutputElement _sum;
-
-    };
-
-    FocalSum()
-    {
-    }
+    FocalSum()=default;
 
     constexpr InputElement fill_value() const
     {
         return 0;
+    }
+
+    template<
+        typename Subspan,
+        typename Kernel>
+    OutputElement operator()(
+        Subspan const& window,
+        Kernel const& kernel) const
+    {
+        static_assert(rank<Kernel> == 2);
+
+        using Weight = ElementT<Kernel>;
+
+        // TODO Add traits to grab typename of elements in Subspan
+        // static_assert(std::is_same_v<ElementT<Subspan>, InputElement>);
+        static_assert(std::is_same_v<Weight, bool> || std::is_floating_point_v<Weight>);
+
+        OutputElement sum{0};
+
+        lue_assert(window.extent(0) == kernel.size());
+        lue_assert(window.extent(1) == kernel.size());
+
+        for(Index r = 0; r < window.extent(0); ++r) {
+            for(Index c = 0; c < window.extent(1); ++c)
+            {
+                Weight const weight{kernel(r, c)};
+                InputElement const value{window(r, c)};
+
+                if constexpr(std::is_same_v<Weight, bool>)
+                {
+                    // TODO Handle no-data
+                    if(weight)
+                    {
+                        sum += value;
+                    }
+                }
+                else
+                {
+                    // TODO Handle no-data
+                    sum += weight * value;
+                }
+            }
+        }
+
+        return sum;
     }
 
 };
