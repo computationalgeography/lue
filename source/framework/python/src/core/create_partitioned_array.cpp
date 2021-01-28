efca1ad0dc1a4b8e13dee30340832edbeeb15d7b
Kor de Jong <k.dejong1@uu.nl>
2021-01-28 23:13:18 +0100

    Add fill_value to create_partitioned_array

--- source/framework/python/src/core/create_partitioned_array.cpp
+++ source/framework/python/src/core/create_partitioned_array.cpp
@@ -6,13 +6,18 @@ namespace lue {
 namespace framework {
 namespace {
 
-using Shape = std::vector<lue::Count>;
+using DynamicShape = std::vector<lue::Count>;
 
-Shape tuple_to_shape(
+template<
+    Rank rank>
+using StaticShape = std::array<lue::Count, rank>;
+
+
+DynamicShape tuple_to_shape(
     pybind11::tuple const& tuple)
 {
     Rank rank{tuple.size()};
-    Shape shape(rank);
+    DynamicShape shape(rank);
 
     for(std::size_t d = 0; d < tuple.size(); ++d)
     {
@@ -28,48 +33,38 @@ template<
     Rank rank>
 PartitionedArray<Element, rank> create_partitioned_array(
     ShapeT<PartitionedArray<Element, rank>> const& array_shape,
-    ShapeT<PartitionedArray<Element, rank>> const& partition_shape)
+    ShapeT<PartitionedArray<Element, rank>> const& partition_shape,
+    pybind11::object const& fill_value)
 {
     // TODO Check that this works asynchronous in Release mode. In Debug
     //     mode it doesn't (yet), because of validation.
-    return PartitionedArray<Element, rank>(array_shape, partition_shape);
+    using Array = PartitionedArray<Element, rank>;
+
+    return Array{array_shape, partition_shape, pybind11::cast<Element>(fill_value), Array::ClampMode::merge};
 }
 
 
 template<
     Rank rank,
-    typename Element>
-std::array<Element, rank> dynamic_shape_to_static_shape(
-    std::vector<Element> const& shape)
+    typename Count>
+std::array<Count, rank> dynamic_shape_to_static_shape(
+    std::vector<Count> const& shape)
 {
     lue_assert(shape.size() == rank);
-    std::array<Element, rank> result{};
+    std::array<Count, rank> result{};
     std::copy(shape.begin(), shape.end(), result.begin());
     return result;
 }
 
 
+template<
+    Rank rank>
 pybind11::object create_partitioned_array(
-    Shape const& array_shape,
-    Shape const& partition_shape,
-    pybind11::dtype const& dtype)
+    StaticShape<rank> const& array_shape,
+    StaticShape<rank> const& partition_shape,
+    pybind11::dtype const& dtype,
+    pybind11::object const& fill_value)
 {
-    // TODO Generalize for more ranks
-
-    if(array_shape.size() != partition_shape.size())
-    {
-        throw std::runtime_error(fmt::format(
-            "Rank of array shape and partition shape must be equal ({} != {})",
-            array_shape.size(), partition_shape.size()));
-    }
-
-    if(array_shape.size() != 2)
-    {
-        throw std::runtime_error("Currently only arrays of rank 2 are supported");
-    }
-
-    constexpr Rank rank{2};
-
     // Switch on dtype and call a function that returns an array of the
     // right value type
     auto const kind = dtype.kind();
@@ -83,28 +78,14 @@ pybind11::object create_partitioned_array(
             // Signed integer
             switch(size)
             {
-                // case 1: {
-                //     result = pybind11::cast(create_partitioned_array<std::int8_t, rank>(
-                //         dynamic_shape_to_static_shape<rank>(array_shape),
-                //         dynamic_shape_to_static_shape<rank>(partition_shape)));
-                //     break;
-                // }
-                // case 2: {
-                //     result = pybind11::cast(create_partitioned_array<std::int16_t, rank>(
-                //         dynamic_shape_to_static_shape<rank>(array_shape),
-                //         dynamic_shape_to_static_shape<rank>(partition_shape)));
-                //     break;
-                // }
                 case 4: {
                     result = pybind11::cast(create_partitioned_array<std::int32_t, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
                 case 8: {
                     result = pybind11::cast(create_partitioned_array<std::int64_t, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
             }
@@ -118,26 +99,17 @@ pybind11::object create_partitioned_array(
             {
                 case 1: {
                     result = pybind11::cast(create_partitioned_array<std::uint8_t, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
-                // case 2: {
-                //     result = pybind11::cast(create_partitioned_array<std::uint16_t, rank>(
-                //         dynamic_shape_to_static_shape<rank>(array_shape),
-                //         dynamic_shape_to_static_shape<rank>(partition_shape)));
-                //     break;
-                // }
                 case 4: {
                     result = pybind11::cast(create_partitioned_array<std::uint32_t, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
                 case 8: {
                     result = pybind11::cast(create_partitioned_array<std::uint64_t, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
             }
@@ -151,31 +123,65 @@ pybind11::object create_partitioned_array(
             {
                 case 4: {
                     result = pybind11::cast(create_partitioned_array<float, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
                 case 8: {
                     result = pybind11::cast(create_partitioned_array<double, rank>(
-                        dynamic_shape_to_static_shape<rank>(array_shape),
-                        dynamic_shape_to_static_shape<rank>(partition_shape)));
+                        array_shape, partition_shape, fill_value));
                     break;
                 }
             }
 
             break;
         }
-        // case 'U': {
-        //     // Unicode string
-        //     break;
-        // }
     }
 
     if(!result)
     {
-        throw std::runtime_error(fmt::format(
-            "Unsupported dtype (kind={}, itemsize={})", kind, size));
+        throw std::runtime_error(fmt::format("Unsupported dtype (kind={}, itemsize={})", kind, size));
     }
+
+    return result;
+}
+
+
+pybind11::object create_partitioned_array(
+    DynamicShape const& array_shape,
+    DynamicShape const& partition_shape,
+    pybind11::dtype const& dtype,
+    pybind11::object const& fill_value)
+{
+    if(array_shape.size() != partition_shape.size())
+    {
+        throw std::runtime_error(fmt::format(
+            "Rank of array shape and partition shape must be equal ({} != {})",
+            array_shape.size(), partition_shape.size()));
+    }
+
+    Rank const rank{array_shape.size()};
+    pybind11::object result;
+
+    if(rank == 1)
+    {
+        result = create_partitioned_array<1>(
+            dynamic_shape_to_static_shape<1>(array_shape),
+            dynamic_shape_to_static_shape<1>(partition_shape),
+            dtype, fill_value);
+    }
+    else if(rank == 2)
+    {
+        result = create_partitioned_array<2>(
+            dynamic_shape_to_static_shape<2>(array_shape),
+            dynamic_shape_to_static_shape<2>(partition_shape),
+            dtype, fill_value);
+    }
+    else
+    {
+        throw std::runtime_error("Currently only arrays of rank 1 or 2 are supported");
+    }
+
+    lue_assert(result);
 
     return result;
 }
@@ -191,10 +197,11 @@ void bind_create_partitioned_array(
         [](
             pybind11::tuple const& array_shape,
             pybind11::tuple const& partition_shape,
-            pybind11::dtype const& dtype)
+            pybind11::dtype const& dtype,
+            pybind11::object const& fill_value)
         {
             return create_partitioned_array(
-                tuple_to_shape(array_shape), tuple_to_shape(partition_shape), dtype);
+                tuple_to_shape(array_shape), tuple_to_shape(partition_shape), dtype, fill_value);
         },
         pybind11::return_value_policy::move);
 }
