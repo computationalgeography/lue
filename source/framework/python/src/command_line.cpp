f9fbf096c45f687d3a270d34b4564f089334c685
Kor de Jong <k.dejong1@uu.nl>
2021-02-11 21:44:16 +0100

    Fix lue.framework unit tests icw Python < 3.9

--- source/framework/python/src/command_line.cpp
+++ source/framework/python/src/command_line.cpp
@@ -11,9 +11,12 @@ CommandLine::CommandLine()
     // The goal here is (only) to set _argc/_argv for the
     // HPX runtime to use
 
-#if PYTHON_VERSION_MAJOR > 2 && PYTHON_VERSION_MINOR > 8
+#if 0 && PYTHON_VERSION_MAJOR > 2 && PYTHON_VERSION_MINOR > 8
     // In Python >= 3.9 a C-API function is exported (again) which allows
     // us access to the original argc/argv passed to the script
+    // TODO(KDJ) This doesn't work correctly when the Python script is
+    //     managed by the Python unittest stuff. Fix this when all the
+    //     rest is done ;-)
     lue_assert(py::BuildOptions::python_version >= 30900);
 
     wchar_t** py_argv;
