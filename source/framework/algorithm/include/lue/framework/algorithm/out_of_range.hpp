0ed712f1b54e217e4954179e263ebe28d8fee89f
Kor de Jong <k.dejong1@uu.nl>
2022-05-30 13:55:20 +0200

    Fix out-of-range detection for signed integers

--- source/framework/algorithm/include/lue/framework/algorithm/out_of_range.hpp
+++ source/framework/algorithm/include/lue/framework/algorithm/out_of_range.hpp
@@ -1,18 +1,66 @@
 #pragma once
-#include <algorithm>
+#include <boost/safe_numerics/safe_integer.hpp>
 #include <cmath>
-#ifndef NDEBUG
-#include <limits>
-#endif
 #include <type_traits>
 
 
 namespace lue {
 
     /*!
-        @brief      Return whether the result of adding @a argument1 and @a argument2 resulted
-                    in an out-of-range @a result value
+        @brief      Add two numbers and return whether the result is valid
     */
+    template<
+        typename T>
+    bool safe_add(
+        T const& argument1,
+        T const& argument2,
+        T& result) noexcept
+    {
+        static_assert(std::is_arithmetic_v<T>);
+
+        if constexpr (std::is_integral_v<T>)
+        {
+            namespace bsf = boost::safe_numerics;
+
+            bool status{false};
+
+            try
+            {
+                bsf::safe<T> a1{argument1};
+                bsf::safe<T> a2{argument2};
+                bsf::safe<T> r{a1 + a2};
+
+                result = r;
+                status = true;
+            }
+            catch([[maybe_unused]] std::exception const& exception)
+            {
+            }
+
+            return status;
+        }
+        else
+        {
+            // Floating point
+            result = argument1 + argument2;
+
+            return std::isinf(result);
+        }
+    }
+
+
+    template<
+        typename T>
+    bool add_within_range(
+        T const& argument1,
+        T const& argument2,
+        [[maybe_unused]] T const& result) noexcept
+    {
+        T r;
+        return safe_add(argument1, argument2, r);
+    }
+
+
     template<
         typename T>
     bool add_out_of_range(
@@ -20,69 +68,65 @@ namespace lue {
         T const& argument2,
         T const& result) noexcept
     {
+        return !add_within_range(argument1, argument2, result);
+    }
+
+
+    /*!
+        @brief      Multiply two numbers and return whether the result is valid
+    */
+    template<
+        typename T>
+    bool safe_multiply(
+        T const& argument1,
+        T const& argument2,
+        T& result) noexcept
+    {
         static_assert(std::is_arithmetic_v<T>);
 
         if constexpr (std::is_integral_v<T>)
         {
-            if constexpr (std::is_unsigned_v<T>)
+            namespace bsf = boost::safe_numerics;
+
+            bool status{false};
+
+            try
             {
-                // Unsigned integer
+                bsf::safe<T> a1{argument1};
+                bsf::safe<T> a2{argument2};
+                bsf::safe<T> r{a1 * a2};
 
-#ifndef NDEBUG
-                static constexpr T max{std::numeric_limits<T>::max()};
-                static_assert(max + 1 == 0);
-#endif
-
-                // Overflow if result is smaller than one of the arguments
-                return result < argument1;  // std::max(argument1, argument2);
+                result = r;
+                status = true;
             }
-            else
+            catch([[maybe_unused]] std::exception const& exception)
             {
-                // Signed integer
-
-#ifndef NDEBUG
-                // static constexpr T min{std::numeric_limits<T>::min()};
-                // static constexpr T max{std::numeric_limits<T>::max()};
-                // TODO Why are these expressions not constant conditions while the one for
-                //      unsigned integers is???
-                // static_assert(min - 1 == max);
-                // static_assert(max + 1 == min);
-#endif
-
-                // Overflow/underflow if sign of result is different
-                return argument2 > 0
-                    ? (result < argument1)
-                    : (result > argument1);
             }
+
+            return status;
         }
         else
         {
             // Floating point
+            result = argument1 * argument2;
 
             return std::isinf(result);
         }
     }
 
 
-    /*!
-        @brief      Return whether the result of adding @a argument1 and @a argument2 resulted
-                    in a within-range @a result value
-    */
     template<
         typename T>
-    bool add_within_range(
+    bool multiply_within_range(
         T const& argument1,
         T const& argument2,
-        T const& result) noexcept
+        [[maybe_unused]] T const& result) noexcept
     {
-        return !add_out_of_range(argument1, argument2, result);
+        T r;
+        return safe_multiply(argument1, argument2, r);
     }
 
 
-    /*!
-        @brief      Return whether the result of multiplying @a argument1 and @a argument2 resulted
-                    in an out-of-range @a result value
-    */
     template<
         typename T>
     bool multiply_out_of_range(
@@ -90,31 +134,7 @@ namespace lue {
         T const& argument2,
         T const& result) noexcept
     {
-        static_assert(std::is_arithmetic_v<T>);
-
-        if constexpr (std::is_integral_v<T>)
-        {
-            return argument2 == T{0} ? false : result / argument2 != argument1;
-        }
-        else
-        {
-            return std::isinf(result);
-        }
-    }
-
-
-    /*!
-        @brief      Return whether the result of multiplying @a argument1 and @a argument2 resulted
-                    in a within-range @a result value
-    */
-    template<
-        typename T>
-    bool multiply_within_range(
-        T const& argument1,
-        T const& argument2,
-        T const& result) noexcept
-    {
-        return !multiply_out_of_range(argument1, argument2, result);
+        return !multiply_within_range(argument1, argument2, result);
     }
 
 }  // namespace lue
--- source/framework/algorithm/test/out_of_range_test.cpp
+++ source/framework/algorithm/test/out_of_range_test.cpp
@@ -7,6 +7,134 @@ namespace {
 
     template<
         typename T>
+    bool safe_add(
+        T const& argument1,
+        T const& argument2)
+    {
+        T result;
+        return lue::safe_add<T>(argument1, argument2, result);
+    }
+
+
+    template<
+        typename T>
+    bool safe_multiply(
+        T const& argument1,
+        T const& argument2)
+    {
+        T result;
+        return lue::safe_multiply<T>(argument1, argument2, result);
+    }
+
+}  // Anonymous namespace
+
+
+BOOST_AUTO_TEST_CASE(safe_add_unsigned_integer)
+{
+    using Element = std::uint32_t;
+
+    auto const max{std::numeric_limits<Element>::max()};
+
+    // In
+    BOOST_CHECK(safe_add<Element>(0, 0));
+    BOOST_CHECK(safe_add<Element>(max, 0));
+    BOOST_CHECK(safe_add<Element>(0, max));
+
+    // Out
+    BOOST_CHECK(!safe_add<Element>(max, 1));
+    BOOST_CHECK(!safe_add<Element>(1, max));
+}
+
+
+BOOST_AUTO_TEST_CASE(safe_add_signed_integer)
+{
+    using Element = std::int32_t;
+
+    auto const min{std::numeric_limits<Element>::min()};
+    auto const max{std::numeric_limits<Element>::max()};
+
+    // In
+    BOOST_CHECK(safe_add<Element>(min, 0));
+    BOOST_CHECK(safe_add<Element>(0, min));
+    BOOST_CHECK(safe_add<Element>(max, 0));
+    BOOST_CHECK(safe_add<Element>(0, max));
+
+    // Out
+    BOOST_CHECK(!safe_add<Element>(min, -1));
+    BOOST_CHECK(!safe_add<Element>(-1, min));
+    BOOST_CHECK(!safe_add<Element>(max, 1));
+    BOOST_CHECK(!safe_add<Element>(1, max));
+}
+
+
+BOOST_AUTO_TEST_CASE(safe_add_floating_point)
+{
+    // using Element = float;
+
+    // auto const smallest{std::numeric_limits<Element>::min()};
+    // auto const lowest{std::numeric_limits<Element>::lowest()};
+    // auto const highest{std::numeric_limits<Element>::max()};
+    // auto const epsilon{std::numeric_limits<Element>::epsilon()};
+
+    // TODO
+}
+
+
+BOOST_AUTO_TEST_CASE(safe_multiply_unsigned_integer)
+{
+    using Element = std::uint32_t;
+
+    auto const max{std::numeric_limits<Element>::max()};
+
+    // In
+    BOOST_CHECK(safe_multiply<Element>(0, 0));
+    BOOST_CHECK(safe_multiply<Element>(max, 1));
+    BOOST_CHECK(safe_multiply<Element>(1, max));
+
+    // Out
+    BOOST_CHECK(!safe_multiply<Element>(max, 2));
+    BOOST_CHECK(!safe_multiply<Element>(2, max));
+}
+
+
+BOOST_AUTO_TEST_CASE(safe_multiply_signed_integer)
+{
+    using Element = std::int32_t;
+
+    auto const min{std::numeric_limits<Element>::min()};
+    auto const max{std::numeric_limits<Element>::max()};
+
+    // In
+    BOOST_CHECK(safe_multiply<Element>(0, 0));
+    BOOST_CHECK(safe_multiply<Element>(min, 1));
+    BOOST_CHECK(safe_multiply<Element>(1, min));
+    BOOST_CHECK(safe_multiply<Element>(max, 1));
+    BOOST_CHECK(safe_multiply<Element>(1, max));
+
+    // Out
+    BOOST_CHECK(!safe_multiply<Element>(min, 2));
+    BOOST_CHECK(!safe_multiply<Element>(2, min));
+    BOOST_CHECK(!safe_multiply<Element>(min, -2));
+    BOOST_CHECK(!safe_multiply<Element>(-2, min));
+    BOOST_CHECK(!safe_multiply<Element>(max, 2));
+    BOOST_CHECK(!safe_multiply<Element>(2, max));
+    BOOST_CHECK(!safe_multiply<Element>(max, -2));
+    BOOST_CHECK(!safe_multiply<Element>(-2, max));
+}
+
+
+BOOST_AUTO_TEST_CASE(safe_multiply_floating_point)
+{
+    // using Element = float;
+
+    // TODO
+}
+
+
+namespace {
+
+    template<
+        typename T>
     bool add_within_range(
         T const& argument1,
         T const& argument2)
@@ -54,13 +182,13 @@ BOOST_AUTO_TEST_CASE(add_unsigned_integer)
     auto const max{std::numeric_limits<Element>::max()};
 
     // In
-    BOOST_CHECK(::add_within_range<Element>(0, 0));
-    BOOST_CHECK(::add_within_range<Element>(max, 0));
-    BOOST_CHECK(::add_within_range<Element>(0, max));
+    BOOST_CHECK(add_within_range<Element>(0, 0));
+    BOOST_CHECK(add_within_range<Element>(max, 0));
+    BOOST_CHECK(add_within_range<Element>(0, max));
 
     // Out
-    BOOST_CHECK(::add_out_of_range<Element>(max, 1));
-    BOOST_CHECK(::add_out_of_range<Element>(1, max));
+    BOOST_CHECK(add_out_of_range<Element>(max, 1));
+    BOOST_CHECK(add_out_of_range<Element>(1, max));
 }
 
 
@@ -105,13 +233,13 @@ BOOST_AUTO_TEST_CASE(multiply_unsigned_integer)
     auto const max{std::numeric_limits<Element>::max()};
 
     // In
-    BOOST_CHECK(::multiply_within_range<Element>(0, 0));
-    BOOST_CHECK(::multiply_within_range<Element>(max, 1));
-    BOOST_CHECK(::multiply_within_range<Element>(1, max));
+    BOOST_CHECK(multiply_within_range<Element>(0, 0));
+    BOOST_CHECK(multiply_within_range<Element>(max, 1));
+    BOOST_CHECK(multiply_within_range<Element>(1, max));
 
     // Out
-    BOOST_CHECK(::multiply_out_of_range<Element>(max, 2));
-    BOOST_CHECK(::multiply_out_of_range<Element>(2, max));
+    BOOST_CHECK(multiply_out_of_range<Element>(max, 2));
+    BOOST_CHECK(multiply_out_of_range<Element>(2, max));
 }
 
 
@@ -123,21 +251,21 @@ BOOST_AUTO_TEST_CASE(multiply_signed_integer)
     auto const max{std::numeric_limits<Element>::max()};
 
     // In
-    BOOST_CHECK(::multiply_within_range<Element>(0, 0));
-    BOOST_CHECK(::multiply_within_range<Element>(min, 1));
-    BOOST_CHECK(::multiply_within_range<Element>(1, min));
-    BOOST_CHECK(::multiply_within_range<Element>(max, 1));
-    BOOST_CHECK(::multiply_within_range<Element>(1, max));
+    BOOST_CHECK(multiply_within_range<Element>(0, 0));
+    BOOST_CHECK(multiply_within_range<Element>(min, 1));
+    BOOST_CHECK(multiply_within_range<Element>(1, min));
+    BOOST_CHECK(multiply_within_range<Element>(max, 1));
+    BOOST_CHECK(multiply_within_range<Element>(1, max));
 
     // Out
-    BOOST_CHECK(::multiply_out_of_range<Element>(min, 2));
-    BOOST_CHECK(::multiply_out_of_range<Element>(2, min));
-    BOOST_CHECK(::multiply_out_of_range<Element>(min, -2));
-    BOOST_CHECK(::multiply_out_of_range<Element>(-2, min));
-    BOOST_CHECK(::multiply_out_of_range<Element>(max, 2));
-    BOOST_CHECK(::multiply_out_of_range<Element>(2, max));
-    BOOST_CHECK(::multiply_out_of_range<Element>(max, -2));
-    BOOST_CHECK(::multiply_out_of_range<Element>(-2, max));
+    BOOST_CHECK(multiply_out_of_range<Element>(min, 2));
+    BOOST_CHECK(multiply_out_of_range<Element>(2, min));
+    BOOST_CHECK(multiply_out_of_range<Element>(min, -2));
+    BOOST_CHECK(multiply_out_of_range<Element>(-2, min));
+    BOOST_CHECK(multiply_out_of_range<Element>(max, 2));
+    BOOST_CHECK(multiply_out_of_range<Element>(2, max));
+    BOOST_CHECK(multiply_out_of_range<Element>(max, -2));
+    BOOST_CHECK(multiply_out_of_range<Element>(-2, max));
 }
 
 
