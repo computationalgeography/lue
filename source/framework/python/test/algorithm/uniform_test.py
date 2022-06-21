f63969fa41008e85dd01220d9d49f4ef5a552bc0
Kor de Jong <k.dejong1@uu.nl>
2022-06-21 18:00:42 +0200

    Fix typo in unit test

--- source/framework/python/test/algorithm/uniform_test.py
+++ source/framework/python/test/algorithm/uniform_test.py
@@ -14,6 +14,23 @@ def tearDownModule():
 class UniformTest(lue_test.TestCase):
 
     @lue_test.framework_test_case
+    def test_overload1(self):
+
+        array_shape = (60, 40)
+        partition_shape = (10, 10)
+        min_value = 33
+        max_value = 55
+        dtype = np.dtype(np.float32)
+        fill_value = 5.5
+
+        # array = lfr.create_array(array_shape, partition_shape, dtype, fill_value)
+
+        # for type_ in [np.uint32, np.uint64, np.int32, np.int64, np.float32, np.float64]:
+        # TODO Support dtype argument
+        #     lfr.uniform(array, min_value, np.dtype(type_), max_value)
+
+
+    @lue_test.framework_test_case
     def test_overload2(self):
 
         array_shape = (60, 40)
@@ -21,5 +38,5 @@ class UniformTest(lue_test.TestCase):
         min_value = 33
         max_value = 55
 
-        for dtype in [np.uint32, np.uint64, np.int32, np.int64, np.float32, np.float64]:
-            lfr.uniform(array_shape, partition_shape, dtype, min_value, max_value)
+        for type_ in [np.uint32, np.uint64, np.int32, np.int64, np.float32, np.float64]:
+            lfr.uniform(array_shape, partition_shape, np.dtype(type_), min_value, max_value)
