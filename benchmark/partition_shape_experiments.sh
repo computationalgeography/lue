6bf5365eb969cfcd00ee26c4a3b9bf720bdd13bf
Kor de Jong <k.dejong1@uu.nl>
2022-06-21 16:49:54 +0200

    Clean-up benchmark stuff

--- benchmark/partition_shape_experiments.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-dirname="$( cd "$(dirname "$BASH_SOURCE")" ; pwd -P )"
-
-source $dirname/common.sh
-parse_command_line $*
-perform_experiment "partition_shape" $benchmark_name $scenario
--- benchmark/script/algorithm/partition_shape_experiments.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-
-dirname="$( cd "$(dirname "$BASH_SOURCE")" ; pwd -P )"
-source $dirname/header.sh
-experiment_name="partition_shape"
-
-
-source $dirname/experiment_footer.sh
