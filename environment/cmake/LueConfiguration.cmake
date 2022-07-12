0e74e24da218a8d1dcac586cc25aa204ebbfb9df
Kor de Jong <k.dejong1@uu.nl>
2022-07-12 07:55:45 +0200

    WIP try to apply HPX patch in GH workflow II

--- environment/cmake/LueConfiguration.cmake
+++ environment/cmake/LueConfiguration.cmake
@@ -617,7 +617,7 @@ if(LUE_HPX_REQUIRED)
             if(EXISTS ${hpx_patch_file})
                 # Get rid of the final warnings in HPX sources
                 set(hpx_patch_command
-                    git apply --reject --ignore-space-change --ignore-whitespace "${hpx_patch_file}")
+                    git apply --check --apply --ignore-space-change --ignore-whitespace "${hpx_patch_file}")
                 message(STATUS "    Applying patch from ${hpx_patch_file}")
             endif()
 
