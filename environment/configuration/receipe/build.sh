cc183daaaa8b39c37935374f832c72b46aff2946
Kor de Jong <k.dejong1@uu.nl>
2022-08-31 16:49:49 +0200

    Refactor

--- environment/configuration/receipe/build.sh
+++ environment/configuration/receipe/build.sh
@@ -14,37 +14,40 @@ cd $SRC_DIR
 
 mkdir -p ../build && cd ../build
 
-CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" cmake $SRC_DIR -G"Ninja" \
--D CMAKE_BUILD_TYPE=Release \
--D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
--D CMAKE_INSTALL_LIBDIR=lib \
--D LUE_INSTALL_PYTHON_PACKAGE_DIR="${SP_DIR}/lue" \
--D Python3_EXECUTABLE="${PYTHON}" \
--D LUE_HAVE_BOOST:BOOL=TRUE \
--D LUE_HAVE_FMT:BOOL=TRUE \
--D LUE_HAVE_PYBIND11:BOOL=TRUE \
--D LUE_HAVE_HDF5:BOOL=TRUE \
--D LUE_HAVE_GDAL:BOOL=TRUE \
--D LUE_HAVE_DOCOPT:BOOL=TRUE \
--D LUE_HAVE_GLFW:BOOL=TRUE \
--D LUE_HAVE_MS_GSL:BOOL=FALSE \
--D LUE_HAVE_NLOHMANN_JSON:BOOL=TRUE \
--D LUE_DATA_MODEL_WITH_PYTHON_API=ON \
--D LUE_DATA_MODEL_WITH_UTILITIES=ON \
--D LUE_BUILD_VIEW=ON \
--D LUE_BUILD_TEST=ON \
--D LUE_BUILD_FRAMEWORK=ON \
--D LUE_FRAMEWORK_WITH_PYTHON_API=ON \
--D LUE_BUILD_HPX=ON \
--D HPX_USE_CMAKE_CXX_STANDARD=ON \
--D HPX_WITH_MALLOC="tcmalloc" \
--D HPX_WITH_PKGCONFIG=OFF \
--D HPX_WITH_EXAMPLES=OFF \
--D HPX_WITH_TESTS=OFF
+PATH=$PREFIX/bin:$PATH CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" \
+    cmake $SRC_DIR -G"Ninja" \
+        -D CMAKE_BUILD_TYPE=Release \
+        -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+        -D CMAKE_INSTALL_LIBDIR=lib \
+        -D LUE_INSTALL_PYTHON_PACKAGE_DIR="${SP_DIR}/lue" \
+        -D Python3_EXECUTABLE="${PYTHON}" \
+        -D LUE_HAVE_BOOST:BOOL=TRUE \
+        -D LUE_HAVE_FMT:BOOL=TRUE \
+        -D LUE_HAVE_PYBIND11:BOOL=TRUE \
+        -D LUE_HAVE_HDF5:BOOL=TRUE \
+        -D LUE_HAVE_GDAL:BOOL=TRUE \
+        -D LUE_HAVE_DOCOPT:BOOL=TRUE \
+        -D LUE_HAVE_GLFW:BOOL=TRUE \
+        -D LUE_HAVE_MS_GSL:BOOL=FALSE \
+        -D LUE_HAVE_NLOHMANN_JSON:BOOL=TRUE \
+        -D LUE_DATA_MODEL_WITH_PYTHON_API=ON \
+        -D LUE_DATA_MODEL_WITH_UTILITIES=ON \
+        -D LUE_BUILD_VIEW=ON \
+        -D LUE_BUILD_TEST=ON \
+        -D LUE_BUILD_FRAMEWORK=ON \
+        -D LUE_FRAMEWORK_WITH_PYTHON_API=ON \
+        -D LUE_BUILD_HPX=ON \
+        -D HPX_USE_CMAKE_CXX_STANDARD=ON \
+        -D HPX_WITH_MALLOC="tcmalloc" \
+        -D HPX_WITH_PKGCONFIG=OFF \
+        -D HPX_WITH_EXAMPLES=OFF \
+        -D HPX_WITH_TESTS=OFF
 
-# Use parallel build but not for lue.framework
-cmake --build . --target lue_view lue_translate lue_validate core
-cmake --build . --target all --parallel 1
+# Use parallel build for as many targets as possible, but not for framework/algorithm
+cmake --build . --target source/view/all source/framework/partitioned_array/all
+
+# Build remaining targets with fewer cores. Compiling these modules requires more memory.
+cmake --build . --target all --parallel 2
 
 ctest --extra-verbose --output-on-failure
 
