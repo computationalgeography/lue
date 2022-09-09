ab84e6793dadfaea7fc991534c8e65d85f37ae0c
Kor de Jong <k.dejong1@uu.nl>
2022-09-09 08:33:22 +0200

    Fix CI, take IX

--- environment/configuration/receipe/bld.bat
+++ environment/configuration/receipe/bld.bat
@@ -48,13 +48,14 @@ cmake %SRC_DIR% ^
     -D LUE_HAVE_PYBIND11=FALSE ^
     -D Boost_USE_STATIC_LIBS=FALSE ^
     -D HDF5_USE_STATIC_LIBRARIES=FALSE ^
-    -D HWLOC_ROOT="%CONDA_PREFIX%/Library" ^
-    -D HWLOC_LIBRARY="%CONDA_PREFIX%/Library/lib/hwloc.dll.lib" ^
     -D Python3_EXECUTABLE="%PYTHON%"
+
+rem -D HWLOC_ROOT="%CONDA_PREFIX%/Library"
+rem -D HWLOC_LIBRARY="%CONDA_PREFIX%/Library/lib/hwloc.dll.lib"
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-# Use parallel build for as many targets as possible, but not for framework/algorithm
+rem Use parallel build for as many targets as possible, but not for framework/algorithm
 cmake --build . --config Release --target all_build
 
 if %errorlevel% neq 0 exit /b %errorlevel%
