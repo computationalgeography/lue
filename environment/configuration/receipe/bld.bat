76f1384d4bd4f71aa1d4c33e3f5b95964ac81369
Kor de Jong <k.dejong1@uu.nl>
2022-09-12 16:21:17 +0200

    Fix CI, take XVI

--- environment/configuration/receipe/bld.bat
+++ environment/configuration/receipe/bld.bat
@@ -48,8 +48,8 @@ cmake %SRC_DIR% ^
     -D LUE_HAVE_PYBIND11=FALSE ^
     -D Boost_USE_STATIC_LIBS=FALSE ^
     -D HDF5_USE_STATIC_LIBRARIES=FALSE ^
-    -D HWLOC_ROOT=%CONDA_PREFIX%/Library ^
-    -D HWLOC_LIBRARY=%CONDA_PREFIX%/Library/hwloc.dll.lib ^
+    -D HWLOC_ROOT="%LIBRARY_LIB%" ^
+    -D HWLOC_LIBRARY="%LIBRARY_LIB%/hwloc.dll.lib" ^
     -D Python3_EXECUTABLE="%PYTHON%"
 
 
