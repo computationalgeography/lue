66e0a7327d9f0f515811293978e1b1c29975c3fe
Kor de Jong <k.dejong1@uu.nl>
2022-10-24 14:23:58 +0200

    Remove unneeded headers

--- source/framework/algorithm/include/lue/framework/algorithm/focal.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "lue/framework/algorithm/focal_maximum.hpp"
-#include "lue/framework/algorithm/focal_mean.hpp"
-#include "lue/framework/algorithm/focal_sum.hpp"
-#include "lue/framework/algorithm/slope.hpp"
