3880ca7bc6589dbee7f156fca928a8eda096e950
Kor de Jong <k.dejong1@uu.nl>
2023-01-31 18:44:26 +0100

    Formatting

--- source/framework/python/test/pcraster/local_operation_test.py
+++ source/framework/python/test/pcraster/local_operation_test.py
@@ -25,9 +25,17 @@ class LocalOperationTest(lue_test.TestCase):
         }
         cls.spatial = {
             np.float32: lfr.create_array(
-                array_shape, partition_shape, np.float32, fill_value=cls.non_spatial[np.float32]),
+                array_shape,
+                partition_shape,
+                np.float32,
+                fill_value=cls.non_spatial[np.float32],
+            ),
             np.float64: lfr.create_array(
-                array_shape, partition_shape, np.float64, fill_value=cls.non_spatial[np.float64]),
+                array_shape,
+                partition_shape,
+                np.float64,
+                fill_value=cls.non_spatial[np.float64],
+            ),
         }
 
     @lue_test.framework_test_case
@@ -80,10 +88,10 @@ class LocalOperationTest(lue_test.TestCase):
         for type_ in [np.float32, np.float64]:
             spatial, non_spatial = self.spatial[type_], self.non_spatial[type_]
 
-            _ = spatial ** spatial
-            _ = spatial ** non_spatial
-            _ = non_spatial ** spatial
-            _ = non_spatial ** non_spatial
+            _ = spatial**spatial
+            _ = spatial**non_spatial
+            _ = non_spatial**spatial
+            _ = non_spatial**non_spatial
 
     @lue_test.framework_test_case
     def test_abs(self):
@@ -92,6 +100,3 @@ class LocalOperationTest(lue_test.TestCase):
 
             _ = lpr.abs(spatial)
             # TODO _ = lpr.abs(non_spatial)
-
-
-
