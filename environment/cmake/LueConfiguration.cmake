b9637d37a87cb139055b5c4704e3938438fb8486
Kor de Jong <k.dejong1@uu.nl>
2023-03-23 12:09:09 +0100

    WIP fixing CI

--- environment/cmake/LueConfiguration.cmake
+++ environment/cmake/LueConfiguration.cmake
@@ -757,6 +757,7 @@ endif()
 
 
 if(LUE_HDF5_REQUIRED)
+    set(HDF5_FIND_DEBUG TRUE)
     find_package(HDF5 REQUIRED COMPONENTS C)
     message(STATUS "HDF5_IS_PARALLEL              : ${HDF5_IS_PARALLEL}")
 endif()
