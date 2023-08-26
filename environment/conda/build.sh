d2cf99d10a69fd6194c88859ff30eadf9feecd55
Kor de Jong <k.dejong1@uu.nl>
2023-08-26 12:36:49 +0200

    Fix C/I, all except Windows Conda

--- environment/conda/build.sh
+++ environment/conda/build.sh
@@ -6,22 +6,39 @@ mkdir build
 
 if [[ $target_platform == linux* ]]; then
     os="Linux"
-elif [[ $target_platform == osx* ]]; then
-    if [[ $target_platform == "osx-64" ]]; then
-        export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    compiler="gcc"
+    libcxx="libstdc++11"
+
+    if [[ $target_platform == "linux-32" ]]; then
+        arch="x86_32"
+    elif [[ $target_platform == "linux-64" ]]; then
+        arch="x86_64"
     fi
+elif [[ $target_platform == osx* ]]; then
+    os="Macos"
+    compiler="apple-clang"
+    libcxx="libc++"
 
     export CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX"
-    os="Macos"
+
+    conan profile detect
+
+    if [[ $target_platform == "osx-64" ]]; then
+        arch="x86_64"
+
+        export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    elif [[ $target_platform == "osx-arm64" ]]; then
+        arch="armv8"
+    fi
 fi
 
 cat > host_profile << EOF
 [settings]
-arch=x86_64
+arch=$arch
 build_type=Release
-compiler=gcc
+compiler=$compiler
 compiler.cppstd=17
-compiler.libcxx=libstdc++11
+compiler.libcxx=$libcxx
 compiler.version=$($CXX -dumpversion | sed 's/\..*//')
 os=$os
 EOF
@@ -30,9 +47,9 @@ cat > build_profile << EOF
 [settings]
 arch=x86_64
 build_type=Release
-compiler=gcc
+compiler=$compiler
 compiler.cppstd=17
-compiler.libcxx=libstdc++11
+compiler.libcxx=$libcxx
 compiler.version=$($CXX_FOR_BUILD -dumpversion | sed 's/\..*//')
 os=$os
 EOF
