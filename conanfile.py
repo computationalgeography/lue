ebf326a11044950d0a28e15db1d51efb1271f5fe
Kor de Jong <k.dejong1@uu.nl>
2023-08-29 16:04:53 +0200

    Update version ranges to resolve conflict

--- conanfile.py
+++ conanfile.py
@@ -24,47 +24,52 @@ class LUERecipe(ConanFile):
 
     def requirements(self):
 
+        # Lower bound of the version ranges should include the versions installed at current
+        # popular Linux distributions (e.g. current Ubuntu LTS).
+        # Upper bound of the version ranges should be left open, or should be set to some
+        # sensible value, known or likely to result in a failed build.
+
         if install_conan_package("boost"):
-            self.requires("boost/[>1.75]")
+            self.requires("boost/[1.74 || >1.75]")
 
         if install_conan_package("docopt.cpp"):
-            self.requires("docopt.cpp/[~0.6]")
+            self.requires("docopt.cpp/[>=0.6.2]")
 
         if install_conan_package("fmt"):
-            self.requires("fmt/[>=9 <10]")
+            self.requires("fmt/[>=8.1.1 <10]")
 
         if install_conan_package("gdal"):
-            self.requires("gdal/[~3.4]")
+            self.requires("gdal/[>=3.4.1]")
 
         if install_conan_package("glfw"):
-            self.requires("glfw/[~3.3]")
+            self.requires("glfw/[>=3.3.6]")
 
         if install_conan_package("hdf5"):
-            self.requires("hdf5/[>=1.12]")
+            self.requires("hdf5/[>=1.10.7]")
 
         if install_conan_package("hwloc"):
-            self.requires("hwloc/[~2.9]")
+            self.requires("hwloc/[>=2.7.0]")
 
         if install_conan_package("imgui"):
             self.requires("imgui/[>=1.89 <2]")
 
         if install_conan_package("mimalloc"):
-            self.requires("mimalloc/[~2.1]")
+            self.requires("mimalloc/[>=2 <3]")
 
         if install_conan_package("nlohmann_json"):
-            self.requires("nlohmann_json/[~3.10]")
+            self.requires("nlohmann_json/[>=3.10]")
 
         if install_conan_package("pybind11"):
-            self.requires("pybind11/[~2.9]")
+            self.requires("pybind11/[>=2.9.1]")
 
         if install_conan_package("span-lite"):
             self.requires("span-lite/[~0.10]")
 
         if install_conan_package("vulkan-headers"):
-            self.requires("vulkan-headers/[~1.3]")
+            self.requires("vulkan-headers/[>=1.3]")
 
         if install_conan_package("vulkan-loader"):
-            self.requires("vulkan-loader/[~1.3]")
+            self.requires("vulkan-loader/[>=1.3]")
 
         def layout(self):
             cmake_layout(self)
