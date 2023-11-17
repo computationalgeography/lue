d0c4499aaa8f20fb88951129d6a4baf4bce222d7
Kor de Jong <kor@jemig.eu>
2023-11-17 15:40:58 +0100

    Fix fetch of fmt

--- environment/cmake/LueConfiguration.cmake
+++ environment/cmake/LueConfiguration.cmake
@@ -578,13 +578,15 @@ if(LUE_GRAPHVIZ_REQUIRED)
 endif()
 
 if(LUE_FMT_REQUIRED)
+    # In fmt, install rules are disabled if it's a subproject
+    set(FMT_INSTALL ON)
     FetchContent_Declare(fmt
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
         GIT_TAG a33701196adfad74917046096bf5a2aa0ab0bb50  # 9.1.0
         SYSTEM
         FIND_PACKAGE_ARGS
     )
-    find_package(fmt REQUIRED)
+    FetchContent_MakeAvailable(fmt)
 endif()
 
 
