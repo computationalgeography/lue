83877d6cc1da4445f5ed7e283ef8498e13d62e64
Kor de Jong <k.dejong1@uu.nl>
2024-03-26 09:42:20 +0100

    Fix compiler warning

--- source/framework/partitioned_route/test/serial_route_test.cpp
+++ source/framework/partitioned_route/test/serial_route_test.cpp
@@ -27,28 +27,28 @@ BOOST_AUTO_TEST_CASE(default_construct)
 }
 
 
-BOOST_AUTO_TEST_CASE(construct)
-{
-    using Route = lue::SerialRoute<std::int64_t, 2>;
-    using Shape = typename Route::Shape;
-    using Starts = typename Route::Starts;
-    using Partitions = typename Route::Partitions;
-
-    Shape const array_shape{60, 40};
-    Shape const shape_in_partitions{6, 4};
-
-    hpx::future<Starts> starts_f{hpx::make_ready_future<Starts>()};
-
-    // TODO Won't work if partitions aren't valid. SerialRoute checks for that.
-
-    // Partitions partitions{shape_in_partitions};
-
-    // Route route{array_shape, std::move(starts_f), std::move(partitions)};
-
-    // BOOST_CHECK_EQUAL(route.shape(), array_shape);
-    // BOOST_CHECK(route.valid());
-    // BOOST_CHECK_EQUAL(route.partitions().shape(), shape_in_partitions);
-    // route.starts().wait();
-    // BOOST_CHECK(route.is_ready());
-    // BOOST_CHECK_EQUAL(route.nr_routes(), 0);
-}
+// BOOST_AUTO_TEST_CASE(construct)
+// {
+//     using Route = lue::SerialRoute<std::int64_t, 2>;
+//     using Shape = typename Route::Shape;
+//     using Starts = typename Route::Starts;
+//     using Partitions = typename Route::Partitions;
+//
+//     Shape const array_shape{60, 40};
+//     Shape const shape_in_partitions{6, 4};
+//
+//     hpx::future<Starts> starts_f{hpx::make_ready_future<Starts>()};
+//
+//     // TODO Won't work if partitions aren't valid. SerialRoute checks for that.
+//
+//     // Partitions partitions{shape_in_partitions};
+//
+//     // Route route{array_shape, std::move(starts_f), std::move(partitions)};
+//
+//     // BOOST_CHECK_EQUAL(route.shape(), array_shape);
+//     // BOOST_CHECK(route.valid());
+//     // BOOST_CHECK_EQUAL(route.partitions().shape(), shape_in_partitions);
+//     // route.starts().wait();
+//     // BOOST_CHECK(route.is_ready());
+//     // BOOST_CHECK_EQUAL(route.nr_routes(), 0);
+// }
