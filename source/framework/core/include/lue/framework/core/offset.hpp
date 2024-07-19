11ce72b689a9b3b93602f5b9b3a6571aea783564
Kor de Jong <k.dejong1@uu.nl>
2024-07-19 11:40:16 +0200

    Port to gcc-14

--- source/framework/core/include/lue/framework/core/offset.hpp
+++ source/framework/core/include/lue/framework/core/offset.hpp
@@ -1,10 +1,11 @@
 #pragma once
+#include "lue/framework/core/define.hpp"  // Rank
 #include <array>
 
 
 namespace lue {
 
-    template<typename Index, Index rank>
+    template<typename Index, Rank rank>
     using Offset = std::array<Index, rank>;
 
 }  // namespace lue
