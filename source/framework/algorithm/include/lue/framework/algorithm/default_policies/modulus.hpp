e817bd4064ead51b789ceeaf6053cdbf2a2bd8e9
saeb-faraji-gargari <saebfaraji@gmail.com>
2024-07-24 19:47:04 +0200

    modulus with policy and primitive test

---
+++ source/framework/algorithm/include/lue/framework/algorithm/default_policies/modulus.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "lue/framework/algorithm/modulus.hpp"
+
+
+namespace lue {
+    namespace policy::modulus {
+
+        template<typename OutputElement, typename InputElement>
+        using DefaultPolicies = policy::DefaultPolicies<
+            DomainPolicy<InputElement>,
+            OutputElements<OutputElement>,
+            InputElements<InputElement, InputElement>>;
+
+    }  // namespace policy::modulus
+
+
+    namespace default_policies {
+
+        LUE_BINARY_LOCAL_OPERATION_OVERLOADS2(modulus, detail::Modulus, policy::modulus::DefaultPolicies)
+        LUE_BINARY_ARITHMETIC_OPERATOR(%, modulus)
+
+    }  // namespace default_policies
+
+}  // namespace lue
