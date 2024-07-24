a4f30f2f0e3950aec1f645dc5673cabc71532f42
saeb-faraji-gargari <saebfaraji@gmail.com>
2024-07-24 19:47:04 +0200

    modulus with policy and primitive test

---
+++ source/framework/algorithm/include/lue/framework/algorithm/modulus.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include "lue/framework/algorithm/binary_local_operation.hpp"
+#include "lue/framework/algorithm/operator.hpp"
+#include "lue/framework/algorithm/policy.hpp"
+
+namespace lue {
+    namespace detail {
+
+        template<typename InputElement, typename OutputElement_ = InputElement>
+        class Modulus
+        {
+            public:
+
+            static_assert(
+                        std::is_integral_v<InputElement>,
+                        "Input arguments for modulus must be an integral type");
+
+                using OutputElement = OutputElement_;
+
+                constexpr OutputElement operator()(
+                    InputElement const& input_element1, InputElement const& input_element2) const noexcept
+                {
+                    return input_element1 % input_element2;
+                }
+        };
+
+    }  // namespace detail
+
+
+    namespace policy::modulus {
+
+        template<typename Element>
+        class DomainPolicy
+        {
+
+            public:
+
+                static constexpr bool within_domain(
+                    [[maybe_unused]] Element const numerator, Element const denominator) noexcept
+                {
+                    return denominator != 0;
+                }
+        };
+
+    }  // namespace policy::modulus
+
+
+    LUE_BINARY_LOCAL_OPERATION_OVERLOADS3(modulus, detail::Modulus)
+
+}  // namespace lue
