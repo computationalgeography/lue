5db6d465294d022f4d3e62bc68392ed2eaca9762
Kor de Jong <k.dejong1@uu.nl>
2024-08-28 10:41:30 +0200

    Split framework Python subpackage

--- source/framework/python/src/algorithm/global_operation.cpp
+++ source/framework/python/src/algorithm/global_operation.cpp
@@ -9,7 +9,7 @@ namespace lue::framework {
     void bind_sum(pybind11::module& module);
 
 
-    void bind_global_operations(pybind11::module& module)
+    PYBIND11_EXPORT void bind_global_operations(pybind11::module& module)
     {
         bind_reclassify(module);
         bind_maximum(module);
