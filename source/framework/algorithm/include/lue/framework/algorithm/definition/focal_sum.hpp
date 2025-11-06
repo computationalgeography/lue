b1e1600dc96bfcfa0d7f7c096ccc93436e67d52b
Kor de Jong <k.dejong1@uu.nl>
2025-11-06 09:45:09 +0100

    Fix, refactor and document focal_sum

--- source/framework/algorithm/include/lue/framework/algorithm/definition/focal_sum.hpp
+++ source/framework/algorithm/include/lue/framework/algorithm/definition/focal_sum.hpp
@@ -7,7 +7,7 @@
 namespace lue {
     namespace detail {
 
-        template<typename InputElement>
+        template<Arithmetic Element>
         class FocalSum
         {
 
@@ -15,21 +15,18 @@ namespace lue {
 
                 static constexpr char const* name{"focal_sum"};
 
-                using OutputElement = InputElement;
+                using InputElement = Element;
+                using OutputElement = Element;
 
 
                 template<typename Kernel, typename OutputPolicies, typename InputPolicies, typename Subspan>
-                OutputElement operator()(
+                auto operator()(
                     Kernel const& kernel,
                     OutputPolicies const& output_policies,
                     InputPolicies const& input_policies,
-                    Subspan const& window) const
+                    Subspan const& window) const -> OutputElement
                 {
-                    static_assert(rank<Kernel> == 2);
-
                     using Weight = ElementT<Kernel>;
-
-                    static_assert(std::is_integral_v<Weight>);
 
                     lue_hpx_assert(window.extent(0) == kernel.size());
                     lue_hpx_assert(window.extent(1) == kernel.size());
@@ -38,32 +35,31 @@ namespace lue {
                     auto const& ondp = output_policies.output_no_data_policy();
 
                     OutputElement sum{0};
+                    bool initialized{false};
 
-                    for (Index r = 0; r < window.extent(0); ++r)
+                    for (Index idx0 = 0; idx0 < window.extent(0); ++idx0)
                     {
-                        for (Index c = 0; c < window.extent(1); ++c)
+                        for (Index idx1 = 0; idx1 < window.extent(1); ++idx1)
                         {
-                            Weight const weight{kernel(r, c)};
-                            InputElement const value{window[r, c]};
+                            Weight const weight{kernel(idx0, idx1)};
+                            InputElement const value{window[idx0, idx1]};
 
-                            if (indp.is_no_data(value))
-                            {
-                                // In case one of the cells within the window contains a no-data
-                                // value, the result is marked as no-data
-                                ondp.mark_no_data(sum);
-                                r = window.extent(0);
-                                c = window.extent(1);
-                            }
-                            else
+                            if (!indp.is_no_data(value))
                             {
                                 if (weight)
                                 {
-                                    // TODO Use out-of-range policy to see whether this succeeds.
-                                    //      Same policy as used by addition.
+                                    // TODO: Use out-of-range policy to see whether this succeeds.
+                                    //       (Same policy as used by addition.)
                                     sum += value;
+                                    initialized = true;
                                 }
                             }
                         }
+                    }
+
+                    if (!initialized)
+                    {
+                        ondp.mark_no_data(sum);
                     }
 
                     return sum;
@@ -73,11 +69,24 @@ namespace lue {
     }  // namespace detail
 
 
-    template<typename Policies, typename Element, Rank rank, typename Kernel>
-    PartitionedArray<Element, rank> focal_sum(
-        Policies const& policies, PartitionedArray<Element, rank> const& array, Kernel const& kernel)
+    /*!
+        @brief      Return an array with per cell the sum of all values within the corresponding focal
+                    neighbourhood
+        @ingroup    focal_operation
+
+        No-data values are filled unless all values within the neighbourhood are no-data.
+    */
+    template<typename Policies, typename Kernel>
+        requires Arithmetic<policy::InputElementT<Policies, 0>> &&
+                 Arithmetic<policy::OutputElementT<Policies, 0>> &&
+                 std::same_as<policy::InputElementT<Policies, 0>, policy::OutputElementT<Policies, 0>> &&
+                 std::integral<ElementT<Kernel>> && (rank<Kernel> == 2)
+    auto focal_sum(
+        Policies const& policies,
+        PartitionedArray<policy::InputElementT<Policies, 0>, 2> const& array,
+        Kernel const& kernel) -> PartitionedArray<policy::OutputElementT<Policies, 0>, 2>
     {
-        using Functor = detail::FocalSum<Element>;
+        using Functor = detail::FocalSum<policy::InputElementT<Policies, 0>>;
 
         return focal_operation(policies, array, kernel, Functor{});
     }
@@ -85,8 +94,8 @@ namespace lue {
 }  // namespace lue
 
 
-#define LUE_INSTANTIATE_FOCAL_SUM(Policies, Element, Kernel)                                                 \
-                                                                                                             \
-    template LUE_FOCAL_OPERATION_EXPORT PartitionedArray<Element, 2>                                         \
-    focal_sum<ArgumentType<void(Policies)>, Element, 2, Kernel>(                                             \
-        ArgumentType<void(Policies)> const&, PartitionedArray<Element, 2> const&, Kernel const&);
+#define LUE_INSTANTIATE_FOCAL_SUM(Policies, Kernel)                                                          \
+    template LUE_FOCAL_OPERATION_EXPORT auto focal_sum<ArgumentType<void(Policies)>, Kernel>(                \
+        ArgumentType<void(Policies)> const&,                                                                 \
+        PartitionedArray<policy::InputElementT<Policies, 0>, 2> const&,                                      \
+        Kernel const&) -> PartitionedArray<policy::OutputElementT<Policies, 0>, 2>;
--- source/framework/algorithm/test/focal_sum_test.cpp
+++ source/framework/algorithm/test/focal_sum_test.cpp
@@ -7,57 +7,92 @@
 #include "lue/framework.hpp"
 
 
-BOOST_AUTO_TEST_CASE(focal_sum_2d_int)
+BOOST_AUTO_TEST_CASE(pcraster_example)
 {
-    using Element = lue::LargestIntegralElement;
+    using Element = lue::LargestSignedIntegralElement;
+    std::size_t const rank = 2;
 
-    if constexpr (sizeof(Element) > 1)
-    {
-        std::size_t const rank = 2;
+    using ElementArray = lue::PartitionedArray<Element, rank>;
+    using Shape = lue::ShapeT<ElementArray>;
 
-        using Array = lue::PartitionedArray<Element, rank>;
-        using Shape = lue::ShapeT<Array>;
+    Shape const array_shape{{5, 5}};
+    Shape const partition_shape{{5, 5}};
 
-        Shape const array_shape{{9, 9}};
-        Shape const partition_shape{{3, 3}};
+    Element const x{lue::policy::no_data_value<Element>};
 
-        // [ 1  2  3  4  5  6  7  8  9]
-        // [10 11 12 13 14 15 16 17 18]
-        // [19 20 21 22 23 24 25 26 27]
-        // [28 29 30 31 32 33 34 35 36]
-        // [37 38 39 40 41 42 43 44 45]
-        // [46 47 48 49 50 51 52 53 54]
-        // [55 56 57 58 59 60 61 62 63]
-        // [64 65 66 67 68 69 70 71 72]
-        // [73 74 75 76 77 78 79 80 81]
-        Array array{lue::create_partitioned_array<Element>(array_shape, partition_shape)};
-        lue::range(array, Element{1}).get();
-
-        // [true true true]
-        // [true true true]
-        // [true true true]
-        auto const kernel = lue::box_kernel<lue::BooleanElement, rank>(1, 1);
-        auto focal_sum = lue::value_policies::focal_sum(array, kernel);
-
-        Element const nd{lue::policy::no_data_value<Element>};
-
-        Array array_we_want = lue::test::create_partitioned_array<Array>(
-            array_shape,
-            partition_shape,
+    auto const array = lue::test::create_partitioned_array<ElementArray>(
+        array_shape,
+        partition_shape,
+        {
+            // NOLINTBEGIN
+            // clang-format off
             {
-                {nd, nd, nd, nd, 99, 108, nd, 180, 189},
-                {nd, nd, nd, 117, 126, 135, 198, 207, 216},
-                {nd, nd, nd, 144, 153, nd, 225, 234, nd},
+                0, -1, 1, -30,  0,
+                2,  x, 1,   2, -3,
+                3,  2, 3,   4,  2,
+                0,  0, 2,  40,  2,
+                1, -2, 4,   7,  1,
+            },
+            // clang-format on
+            // NOLINTEND
+        });
+    auto const kernel = lue::box_kernel<lue::BooleanElement, 2>(1, 1);
+    auto const result_we_got = lue::value_policies::focal_sum(array, kernel);
+    auto const result_we_want = lue::test::create_partitioned_array<ElementArray>(
+        array_shape,
+        partition_shape,
+        {
+            // NOLINTBEGIN
+            // clang-format off
+            {
+                1, 3, -27, -29, -31,
+                6, 11, -18, -20, -25,
+                7, 13, 54, 53, 47,
+                4, 13, 60, 65, 56,
+                -1, 5, 51, 56, 50,
+            },
+            // clang-format on
+            // NOLINTEND
+        });
 
-                {nd, 261, 270, nd, 342, 351, nd, 423, 432},
-                {279, 288, 297, 360, 369, 378, 441, 450, 459},
-                {306, 315, nd, 387, 396, nd, 468, 477, nd},
+    lue::test::check_arrays_are_equal(result_we_got, result_we_want);
+}
 
-                {nd, 504, 513, nd, 585, 594, nd, nd, nd},
-                {522, 531, 540, 603, 612, 621, nd, nd, nd},
-                {549, 558, nd, 630, 639, nd, nd, nd, nd},
-            });
 
-        lue::test::check_arrays_are_equal(focal_sum, array_we_want);
-    }
+BOOST_AUTO_TEST_CASE(all_no_data)
+{
+    using Element = lue::LargestSignedIntegralElement;
+    std::size_t const rank = 2;
+
+    using ElementArray = lue::PartitionedArray<Element, rank>;
+    using Shape = lue::ShapeT<ElementArray>;
+
+    Shape const array_shape{{3, 3}};
+    Shape const partition_shape{{3, 3}};
+
+    Element const x{lue::policy::no_data_value<Element>};
+
+    // x x x   x x x
+    // x x x → x x x
+    // x x x   x x x
+    auto const array{lue::create_partitioned_array<Element>(array_shape, partition_shape, x)};
+    auto const kernel = lue::box_kernel<lue::BooleanElement, rank>(1, 1);
+    auto const result_we_got = lue::value_policies::focal_sum(array, kernel);
+
+    auto const result_we_want = lue::test::create_partitioned_array<ElementArray>(
+        array_shape,
+        partition_shape,
+        {
+            // NOLINTBEGIN
+            // clang-format off
+            {
+                x, x, x,
+                x, x, x,
+                x, x, x,
+            },
+            // clang-format on
+            // NOLINTEND
+        });
+
+    lue::test::check_arrays_are_equal(result_we_got, result_we_want);
 }
