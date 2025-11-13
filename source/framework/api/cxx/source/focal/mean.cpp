21b65135d1b3914d5995c72bd5d3df8f6628c391
Kor de Jong <k.dejong1@uu.nl>
2025-11-13 18:09:15 +0100

    Port to moved api headers

--- source/framework/api/cxx/source/focal/mean.cpp
+++ source/framework/api/cxx/source/focal/mean.cpp
@@ -1,7 +1,7 @@
 #include "lue/framework/api/cxx/focal/mean.hpp"
-#include "lue/framework/algorithm/value_policies/focal_mean.hpp"
-#include "lue/framework/api/cxx/detail/overload.hpp"
 #include "lue/framework/api/cxx/detail/unsupported_overload.hpp"
+#include "lue/framework/algorithm/value_policies/focal_mean.hpp"
+#include "lue/overload.hpp"
 
 
 namespace lue {
