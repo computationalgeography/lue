234b5021f89417c997c84f770de7d111de433f35
Kor de Jong <k.dejong1@uu.nl>
2025-11-18 15:35:59 +0100

    Update version spec in find_package of HPX

--- environment/cmake/LueConfiguration.cmake
+++ environment/cmake/LueConfiguration.cmake
@@ -522,7 +522,7 @@ endif()
 
 
 if(LUE_HPX_REQUIRED)
-    find_package(HPX 1.11...<1.12 REQUIRED)
+    find_package(HPX 1.11 REQUIRED)
 
     if(HPX_FOUND)
         message(STATUS "Using HPX ${HPX_VERSION} found in ${HPX_PREFIX}")
