1b34f210d52e75227bcf96695a7471302168edb7
Kor de Jong <k.dejong1@uu.nl>
2025-11-26 13:34:20 +0100

    Move export macro to header

--- source/framework/io/include/lue/framework/io/lue.hpp
+++ source/framework/io/include/lue/framework/io/lue.hpp
@@ -10,15 +10,16 @@ namespace lue::detail {
     using OpenFileSerializer = Serializer<std::filesystem::path, Count>;
 
 
-    auto open_file_serializer() -> OpenFileSerializer&;
+    LUE_FRAMEWORK_IO_EXPORT auto open_file_serializer() -> OpenFileSerializer&;
 
-    auto normalize(std::string const& pathname) -> std::filesystem::path;
+    LUE_FRAMEWORK_IO_EXPORT auto normalize(std::string const& pathname) -> std::filesystem::path;
 
-    auto open_file_promise_for(std::filesystem::path const& path, Count open_count) -> hpx::promise<void>;
+    LUE_FRAMEWORK_IO_EXPORT auto open_file_promise_for(std::filesystem::path const& path, Count open_count)
+        -> hpx::promise<void>;
 
-    auto open_file_when_predecessor_done(std::filesystem::path const& path, Count open_count)
-        -> hpx::future<void>;
+    LUE_FRAMEWORK_IO_EXPORT auto open_file_when_predecessor_done(
+        std::filesystem::path const& path, Count open_count) -> hpx::future<void>;
 
-    auto open_count(std::filesystem::path const& dataset_path) -> Count;
+    LUE_FRAMEWORK_IO_EXPORT auto open_count(std::filesystem::path const& dataset_path) -> Count;
 
 }  // namespace lue::detail
