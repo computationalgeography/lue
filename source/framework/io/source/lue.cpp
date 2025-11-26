d9e7986ec6f49b9c55731f754a5bfb045038c307
Kor de Jong <k.dejong1@uu.nl>
2025-11-26 13:34:20 +0100

    Move export macro to header

--- source/framework/io/source/lue.cpp
+++ source/framework/io/source/lue.cpp
@@ -3,7 +3,7 @@
 
 namespace lue::detail {
 
-    LUE_FRAMEWORK_IO_EXPORT auto open_file_serializer() -> OpenFileSerializer&
+    auto open_file_serializer() -> OpenFileSerializer&
     {
         static Serializer<std::filesystem::path, Count> open_file_serializer{};
 
@@ -11,21 +11,21 @@ namespace lue::detail {
     }
 
 
-    LUE_FRAMEWORK_IO_EXPORT auto normalize(std::string const& pathname) -> std::filesystem::path
+    auto normalize(std::string const& pathname) -> std::filesystem::path
     {
         return std::filesystem::canonical(pathname);
     }
 
 
-    LUE_FRAMEWORK_IO_EXPORT auto open_file_promise_for(
-        std::filesystem::path const& path, Count const open_count) -> hpx::promise<void>
+    auto open_file_promise_for(std::filesystem::path const& path, Count const open_count)
+        -> hpx::promise<void>
     {
         return open_file_serializer().promise_for(path, open_count);
     }
 
 
-    LUE_FRAMEWORK_IO_EXPORT auto open_file_when_predecessor_done(
-        std::filesystem::path const& path, Count const open_count) -> hpx::future<void>
+    auto open_file_when_predecessor_done(std::filesystem::path const& path, Count const open_count)
+        -> hpx::future<void>
     {
         return open_file_serializer().when_predecessor_done(path, open_count);
     }
@@ -36,7 +36,7 @@ namespace lue::detail {
 
         This count can be used to order / serialize open_dataset calls
     */
-    LUE_FRAMEWORK_IO_EXPORT auto open_count(std::filesystem::path const& dataset_path) -> Count
+    auto open_count(std::filesystem::path const& dataset_path) -> Count
     {
         static std::map<std::filesystem::path, Count> open_count_by_dataset_path{};
 
