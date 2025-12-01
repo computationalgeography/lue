4ddfd0cf7687789081302752124bc3d1e84ebfbc
Kor de Jong <k.dejong1@uu.nl>
2025-12-01 09:51:47 +0100

    pre-commit fixes
    Refactor, update clang-tidy version and config, turn checks on hdf5
    library on

--- source/data_model/hdf5/include/lue/hdf5/validate.hpp
+++ source/data_model/hdf5/include/lue/hdf5/validate.hpp
@@ -2,21 +2,21 @@
 #include "lue/hdf5/issues.hpp"
 
 
-// TODO(KDJ) This module is not needed anymore, I think. Remove it if so.
-//      Move issue/issues modules to lue_cxx, where they belong.
-namespace lue {
-    namespace hdf5 {
+// TODO: This module is not needed anymore, I think. Remove it if so.
+//       Move issue/issues modules to lue_cxx, where they belong.
 
-        class Dataset;
-        class Group;
+namespace lue::hdf5 {
+
+    class Dataset;
+    class Group;
 
 
-        void assert_no_superfluous_objects_exist(
-            Group const& group, std::vector<std::string> const& names, Issues& issues);
+    void assert_no_superfluous_objects_exist(
+        Group const& group, std::vector<std::string> const& names, Issues& issues);
 
-        bool assert_groups_exist(Group const& group, std::vector<std::string> const& names, Issues& issues);
+    auto assert_groups_exist(Group const& group, std::vector<std::string> const& names, Issues& issues)
+        -> bool;
 
-        void assert_unique_values(Dataset const& dataset, Issues& issues);
+    void assert_unique_values(Dataset const& dataset, Issues& issues);
 
-    }  // namespace hdf5
-}  // namespace lue
+}  // namespace lue::hdf5
