76a8c69b3c4c6f2844a7fa2608ce5b0b7a402852
Kor de Jong <k.dejong1@uu.nl>
2026-09-16 13:41:52 +0200

    Use installed nlohmann_json if available

--- environment/cmake/LueConfiguration.cmake
+++ environment/cmake/LueConfiguration.cmake
@@ -759,6 +759,7 @@ if(LUE_NLOHMANN_JSON_REQUIRED)
         GIT_REPOSITORY https://github.com/nlohmann/json.git
         GIT_TAG 9cca280a4d0ccf0c08f47a99aa71d1b0e52f8d03  # v3.11.3
         SYSTEM
+        FIND_PACKAGE_ARGS 3.11 CONFIG
     )
     FetchContent_MakeAvailable(nlohmann_json)
 endif()
